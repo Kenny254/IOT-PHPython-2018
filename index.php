976e50f3b636cee337e53f4a4df56ac783a62cb8
Ken Ronoh <kennethronoh1@gmail.com>
2018-10-09 01:20:08 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<HTML>
+<TITLE></TITLE>
+<BODY>
+<DIV>
+<form method="post">
+            <h4 class="card-title">lighting</h4>
+            <h5 class="card-subtitle">Device status: <strong>OFF</strong></h5>
+            <p class="card-text">Switch Lights On/Off using the switches bellow.</p>
+            <div class="card-btn"><a  name="LightON" class="btn btn-warning">ON</a></div>
+						<div class="card-btn"><a  name="LightOFF" class="btn btn-danger">OFF</a></div>
+            </div>
+                
+</form>
+
+</DIV>
+</BODY>
+</HTML>
